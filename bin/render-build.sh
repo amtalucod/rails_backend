8c2971e3d78c26e53158a1f11ee779aa6252f7cd
amtalucod <alfredtalucod@yahoo.com>
2024-05-08 14:30:30 +0800

    add user seeds

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 #bundle exec rails db:reset
-#bundle exec rails db:seed
+bundle exec rails db:seed
 bundle exec rails db:migrate
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-User.create!(name: "Admin User",
-                email: "admin1@sample.com",
-                # location_attributes: { country: "Philippines",
-                #                         region: "Region III",
-                #                         city: "Hagonoy"},
-                password: "123456",
-                password_confirmation: "123456",
-                admin: true)
+User.create!(
+  first_name: "Super",
+  last_name: "Admin",
+  email: "superadmin01@sample.com",
+  mobile: "09123456789",
+  password: "123456",
+  password_confirmation: "123456",
+  admin: true
+)
+
+User.create!(
+  first_name: "John",
+  last_name: "Smith",
+  email: "test01@sample.com",
+  mobile: "09123456788",
+  password: "123456",
+  password_confirmation: "123456",
+  admin: false
+)
