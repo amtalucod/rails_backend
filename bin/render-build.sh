b6a653eb9bf39e1a677d5a9d7446280e1d77780c
amtalucod <alfredtalucod@yahoo.com>
2024-05-08 14:56:56 +0800

    rake1

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,8 +5,8 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:reset
-
+# bundle exec rails db:reset
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rake db:reset db:seed
 bundle exec rails db:migrate
 # bundle exec rails db:seed
 
