5c33cfe6f0db82ac74f4e5c988d37783fb2d7baa
amtalucod <alfredtalucod@yahoo.com>
2024-05-08 14:33:45 +0800

    add user seeds mobile number

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ User.create!(
   first_name: "Super",
   last_name: "Admin",
   email: "superadmin01@sample.com",
-  mobile: "09123456789",
+  mobile_number: "09123456789",
   password: "123456",
   password_confirmation: "123456",
   admin: true
@@ -12,7 +12,7 @@ User.create!(
   first_name: "John",
   last_name: "Smith",
   email: "test01@sample.com",
-  mobile: "09123456788",
+  mobile_number: "09123456788",
   password: "123456",
   password_confirmation: "123456",
   admin: false
